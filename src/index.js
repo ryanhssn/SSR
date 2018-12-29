5d65242397791dbacd14e4bb98e93cf799a62d31
Bilal H <hussain.bilal000786@gmail.com>
2018-12-30 01:45:38 +0530

    create store on both client and server side && added redux on client side

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import 'babel-polyfill';
 import express from 'express'
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
@@ -7,8 +9,11 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-   
-    res.send(renderer(req))
+    const store = createStore();
+    
+    // some logic to initialize and load some data into the store
+
+    res.send(renderer(req, store));
 
 })
 
